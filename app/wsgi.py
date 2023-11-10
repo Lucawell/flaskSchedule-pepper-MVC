889bfc5ae0be0d8fd7ad17733cd9d143c89a5ae4
luca <434203702@qq.com>
2023-11-10 19:30:42 +0800

    增加wsgi内容

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,1 +1,9 @@
 # encoding:utf-8
+import sys
+
+sys.path.append('D:/Projects_D/pepper_robot/flask-mvc-pepper-schedule')
+from app import create_app
+
+app = create_app()
+if __name__ == "__main__":
+    app.run()
