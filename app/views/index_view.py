da28ab814ab730ce31615d3f0260c1382bef23bc
luca <434203702@qq.com>
2023-11-24 19:34:05 +0800

    增加主页重定向

--- app/views/index_view.py
+++ app/views/index_view.py
@@ -1,8 +1,10 @@
 # encoding:utf-8
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 index_blueprint = Blueprint('index', __name__)
 
 @index_blueprint.route('/')
 def home():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return redirect(url_for('event.show_event'))  # 重定向到事件列表页面或其他适当的页面
+
