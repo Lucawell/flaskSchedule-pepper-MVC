abba66149e619110c2fd742db211ea4816b23e00
luca <434203702@qq.com>
2023-12-01 16:48:41 +0800

    增加邮件历史功能(生成,查询,删除);增加EmailHistory数据表

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,7 @@ login_manager = LoginManager()  # 登录管理实例
 migrate = Migrate()  # 数据库迁移实例
 mail = Mail()  # 邮件实例
 
+
 def create_app() -> Flask:
     """创建Flask应用实例并进行初始化设置。
 
@@ -30,6 +31,7 @@ def create_app() -> Flask:
 
     return app
 
+
 def initialize_extensions(app: Flask) -> None:
     """初始化扩展，包括数据库、登录管理、数据库迁移、邮件和文件上传。
 
@@ -44,6 +46,7 @@ def initialize_extensions(app: Flask) -> None:
 
     from app.controller.api_controller import photos
     configure_uploads(app, photos)
+
 
 def register_blueprints(app: Flask) -> None:
     """注册蓝图，包括主页、用户、事件和提醒蓝图。
@@ -60,6 +63,7 @@ def register_blueprints(app: Flask) -> None:
     app.register_blueprint(user_blueprint, url_prefix='/user')
     app.register_blueprint(event_blueprint, url_prefix='/event')
     app.register_blueprint(reminder_blueprint, url_prefix='/reminder')
+
 
 def configure_api_resources(app: Flask) -> None:
     """配置API资源，包括事件、消息、邮件、短信和文件上传资源。
@@ -79,16 +83,20 @@ def configure_api_resources(app: Flask) -> None:
     api.add_resource(SMSResource, '/api/send-sms')
     api.add_resource(FileUploadResource, '/api/upload')
 
+
 def configure_admin(app: Flask) -> None:
     """配置Flask-Admin管理界面，包括自定义首页和用户、事件管理视图。
 
     Args:
         app (Flask): Flask应用实例。
     """
-    from app.views.admin_view import MyAdminIndexView, UserAdminView, EventAdminView
+    from app.views.admin_view import MyAdminIndexView, UserAdminView, EventAdminView, EmailHistoryAdminView
     from app.model.User import User
     from app.model.Event import Event
+    from app.model.EmailHistory import EmailHistory
 
     admin = Admin(app, index_view=MyAdminIndexView())
     admin.add_view(UserAdminView(User, db.session, url='users', name='users', endpoint='users_admin'))
     admin.add_view(EventAdminView(Event, db.session, url='events', name='events', endpoint='events_admin'))
+    admin.add_view(EmailHistoryAdminView(EmailHistory, db.session, url='email-history', name='email-history',
+                                         endpoint='email_history_admin'))
